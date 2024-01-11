33e8dfaf2a6fc20d5f5525aca3f68e830472a2b8
Maxwell0309 <2645596648@qq.com>
2024-01-11 16:15:16 +1030

    2

--- OOP Assignment2.py
+++ OOP Assignment2.py
@@ -16,3 +16,22 @@ def main_menu():
             break
         else:
             print("Wrong input, must be a number between 1 and 3 ")
+
+def circuit_kits_menu():
+    while True:
+        print("CIRCUIT KIT MENU")
+        print("1. NEW CIRCUIT KIT")
+        print("2. VIEW CIRCUIT KITS")
+        print("3. BACK")
+        choice = input("Please enter a number: ")
+
+        if choice == '1':
+            new_circuit_kit_menu()
+            pass
+        elif choice == '2':
+            view_circuit_kits()
+            pass
+        elif choice == '3':
+            break
+        else:
+            print("Wrong input, must be a number between 1 and 3")
