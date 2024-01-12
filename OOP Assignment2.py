45e3cc3ea464e9bac57dcd8b3e9bd8ce4430ee7a
Maxwell0309 <2645596648@qq.com>
2024-01-12 19:32:55 +1030

    b

--- OOP Assignment2.py
+++ OOP Assignment2.py
@@ -142,9 +142,7 @@ def new_circuit_kit_menu():
             print("Wrong input, must be a number between 1 and 9")
 
 def create_light_globe_circuit_kit():
-
     print("LIGHT GLOBE CIRCUIT KIT")
-
 
     light_globes = [
         "6.5V 240.0mA Warm Light Globe $3.50 x 11",
@@ -152,55 +150,35 @@ def create_light_globe_circuit_kit():
         "7.6V 280.0mA Cool Light Globe $3.70 x 6",
         "6.5V 240.0mA Cool Light Globe $3.50 x 4"
     ]
-    for i, globe in enumerate(light_globes, 1):
-        print(f"{i}. {globe}")
-    globe_choice = input("Please select the light globe index: ")
+    selected_globe, globe_quantity = select_component(light_globes)
 
-
-    globe_quantity = input(f"Please enter number of {light_globes[int(globe_choice)-1]}: ")
-
-    print("Select type of switch")
     switches = [
         "4.5V Push Switch $4.60 x 12",
         "4.5V Toggle Switch $4.80 x 1"
     ]
-    for i, switch in enumerate(switches, 1):
-        print(f"{i}. {switch}")
-    switch_choice = input("Please select the switch index: ")
+    selected_switch, switch_quantity = select_component(switches)
 
-    switch_quantity = input(f"Please enter number of {switches[int(switch_choice)-1]}: ")
-
-    print("Select type of battery")
     batteries = [
         "1.2V AA Battery $2.60 x 12",
         "1.5V AAA Battery $2.70 x 8",
         "1.5V AA Battery $3.10 x 15",
         "1.5V AAA Battery $3.10 x 1"
     ]
-    for i, battery in enumerate(batteries, 1):
-        print(f"{i}. {battery}")
-    battery_choice = input("Please select the battery index: ")
+    selected_battery, battery_quantity = select_component(batteries)
 
-    battery_quantity = input(f"Please enter number of {batteries[int(battery_choice)-1]}: ")
-
-    print("Select type of wire")
     wires = [
         "40mm Wire $2.40 x 24",
         "60mm Wire $3.20 x 56",
         "80mm Wire $4.60 x 25"
     ]
-    for i, wire in enumerate(wires, 1):
-        print(f"{i}. {wire}")
-    wire_choice = input("Please select the wire index: ")
-
-    wire_quantity = input(f"Please enter number of {wires[int(wire_choice)-1]}: ")
+    selected_wire, wire_quantity = select_component(wires)
 
     print("Creating Light Globe Circuit Kit with:")
-    print(light_globes[int(globe_choice)-1], "x", globe_quantity)
-    print(switches[int(switch_choice)-1], "x", switch_quantity)
-    print(batteries[int(battery_choice)-1], "x", battery_quantity)
-    print(wires[int(wire_choice)-1], "x", wire_quantity)
-
+    print(selected_globe, "x", globe_quantity)
+    print(selected_switch, "x", switch_quantity)
+    print(selected_battery, "x", battery_quantity)
+    print(selected_wire, "x", wire_quantity)
+    
 def create_led_light_circuit_kit():
     print("LED LIGHT CIRCUIT KIT")
 
