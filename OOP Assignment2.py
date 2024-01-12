855bb2ace66f0f527f8f965d7f25d663029c78ce
Maxwell0309 <2645596648@qq.com>
2024-01-12 19:34:11 +1030

    v

--- OOP Assignment2.py
+++ OOP Assignment2.py
@@ -227,11 +227,7 @@ def create_sensor_circuit_kit_with_light_globe():
         "6.0V Infrared Sensor $4.70 x 4",
         "5.0V Dust Sensor $3.90 x 1"
     ]
-    for i, sensor in enumerate(sensors, 1):
-        print(f"{i}. {sensor}")
-    sensor_choice = input("Please select the sensor index: ")
-
-    sensor_quantity = input(f"Please enter number of {sensors[int(sensor_choice)-1]}: ")
+    selected_sensor, sensor_quantity = select_component(sensors)
 
     light_globes = [
         "6.5V 240.0mA Warm Light Globe $3.50 x 11",
@@ -239,57 +235,37 @@ def create_sensor_circuit_kit_with_light_globe():
         "7.6V 280.0mA Cool Light Globe $3.70 x 6",
         "6.5V 240.0mA Cool Light Globe $3.50 x 4"
     ]
-    for i, globe in enumerate(light_globes, 1):
-        print(f"{i}. {globe}")
-    globe_choice = input("Please select the light globe index: ")
+    selected_globe, globe_quantity = select_component(light_globes)
 
-    globe_quantity = input(f"Please enter number of {light_globes[int(globe_choice)-1]}: ")
-
-    print("Select type of switch")
     switches = [
         "4.5V Push Switch $4.60 x 12",
         "4.5V Toggle Switch $4.80 x 1"
     ]
-    for i, switch in enumerate(switches, 1):
-        print(f"{i}. {switch}")
-    switch_choice = input("Please select the switch index: ")
+    selected_switch, switch_quantity = select_component(switches)
 
-    switch_quantity = input(f"Please enter number of {switches[int(switch_choice)-1]}: ")
-
-    print("Select type of battery")
     batteries = [
         "1.2V AA Battery $2.60 x 12",
         "1.5V AAA Battery $2.70 x 8",
         "1.5V AA Battery $3.10 x 15",
         "1.5V AAA Battery $3.10 x 1"
     ]
-    for i, battery in enumerate(batteries, 1):
-        print(f"{i}. {battery}")
-    battery_choice = input("Please select the battery index: ")
+    selected_battery, battery_quantity = select_component(batteries)
 
-    battery_quantity = input(f"Please enter number of {batteries[int(battery_choice)-1]}: ")
-
-    print("Select type of wire")
     wires = [
         "40mm Wire $2.40 x 24",
         "60mm Wire $3.20 x 56",
         "80mm Wire $4.60 x 25"
     ]
-    for i, wire in enumerate(wires, 1):
-        print(f"{i}. {wire}")
-    wire_choice = input("Please select the wire index: ")
-
-    wire_quantity = input(f"Please enter number of {wires[int(wire_choice)-1]}: ")
+    selected_wire, wire_quantity = select_component(wires)
 
     print("Creating Sensor Circuit Kit with Light Globe:")
-    print(sensors[int(sensor_choice)-1], "x", sensor_quantity)
-    print(light_globes[int(globe_choice)-1], "x", globe_quantity)
-    print(switches[int(switch_choice)-1], "x", switch_quantity)
-    print(batteries[int(battery_choice)-1], "x", battery_quantity)
-    print(wires[int(wire_choice)-1], "x", wire_quantity)
+    print(selected_sensor, "x", sensor_quantity)
+    print(selected_globe, "x", globe_quantity)
+    print(selected_switch, "x", switch_quantity)
+    print(selected_battery, "x", battery_quantity)
+    print(selected_wire, "x", wire_quantity)
 
 def create_sensor_circuit_kit_with_led_light():
-
     print("SENSOR CIRCUIT KIT WITH LED LIGHT")
 
     sensors = [
@@ -297,11 +273,7 @@ def create_sensor_circuit_kit_with_led_light():
         "6.0V Infrared Sensor $4.70 x 4",
         "5.0V Dust Sensor $3.90 x 1"
     ]
-    for i, sensor in enumerate(sensors, 1):
-        print(f"{i}. {sensor}")
-    sensor_choice = input("Please select the sensor index: ")
-
-    sensor_quantity = input(f"Please enter number of {sensors[int(sensor_choice)-1]}: ")
+    selected_sensor, sensor_quantity = select_component(sensors)
 
     led_lights = [
         "3.0V 150.0mA Red LED Light $2.20 x 20",
@@ -311,53 +283,35 @@ def create_sensor_circuit_kit_with_led_light():
         "6.5V 280.0mA Green LED Light $3.50 x 4",
         "6.5V 240.0mA Red LED Light $3.50 x 5"
     ]
-    for i, led in enumerate(led_lights, 1):
-        print(f"{i}. {led}")
-    led_choice = input("Please select the LED light index: ")
-    led_quantity = input(f"Please enter number of {led_lights[int(led_choice)-1]}: ")
+    selected_led, led_quantity = select_component(led_lights)
 
-    print("Select type of switch")
     switches = [
         "4.5V Push Switch $4.60 x 12",
         "4.5V Toggle Switch $4.80 x 1"
     ]
-    for i, switch in enumerate(switches, 1):
-        print(f"{i}. {switch}")
-    switch_choice = input("Please select the switch index: ")
+    selected_switch, switch_quantity = select_component(switches)
 
-    switch_quantity = input(f"Please enter number of {switches[int(switch_choice)-1]}: ")
-
-    print("Select type of battery")
     batteries = [
         "1.2V AA Battery $2.60 x 12",
         "1.5V AAA Battery $2.70 x 8",
         "1.5V AA Battery $3.10 x 15",
         "1.5V AAA Battery $3.10 x 1"
     ]
-    for i, battery in enumerate(batteries, 1):
-        print(f"{i}. {battery}")
-    battery_choice = input("Please select the battery index: ")
+    selected_battery, battery_quantity = select_component(batteries)
 
-    battery_quantity = input(f"Please enter number of {batteries[int(battery_choice)-1]}: ")
-
-    print("Select type of wire")
     wires = [
         "40mm Wire $2.40 x 24",
         "60mm Wire $3.20 x 56",
         "80mm Wire $4.60 x 25"
     ]
-    for i, wire in enumerate(wires, 1):
-        print(f"{i}. {wire}")
-    wire_choice = input("Please select the wire index: ")
-
-    wire_quantity = input(f"Please enter number of {wires[int(wire_choice)-1]}: ")
+    selected_wire, wire_quantity = select_component(wires)
 
     print("Creating Sensor Circuit Kit with LED Light:")
-    print(sensors[int(sensor_choice)-1], "x", sensor_quantity)
-    print(led_lights[int(led_choice)-1], "x", led_quantity)
-    print(switches[int(switch_choice)-1], "x", switch_quantity)
-    print(batteries[int(battery_choice)-1], "x", battery_quantity)
-    print(wires[int(wire_choice)-1], "x", wire_quantity)
+    print(selected_sensor, "x", sensor_quantity)
+    print(selected_led, "x", led_quantity)
+    print(selected_switch, "x", switch_quantity)
+    print(selected_battery, "x", battery_quantity)
+    print(selected_wire, "x", wire_quantity)
 
 def create_sensor_circuit_kit_with_buzzer():
     print("SENSOR CIRCUIT KIT WITH BUZZER")
